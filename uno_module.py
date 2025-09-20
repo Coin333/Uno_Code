e0ba386fcac7f2f320a95cf5e9fa61a15e7e24d9
Coin333 <116315051+Coin333@users.noreply.github.com>
2025-09-20 10:03:54 -0700

    Update uno_module.py

--- uno_module.py
+++ uno_module.py
@@ -166,5 +166,5 @@ def game(players,cardinplay):
     print("Remaining players:",[player['name'] for player in total_players])
     winner = ["Colin"]
     print("Winning players:",winner)
-    os._exit(0)
 game(playersx,cardinplayx)
+os._exit(0)
